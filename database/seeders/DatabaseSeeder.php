f935a4cb1f0aa2d7c77edb32849375409ad60256
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2024-12-01 21:37:04 +0000

    Create authification

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-       // User::factory()->create([
-         //   'name' => 'Test User',
-           // 'email' => 'test@example.com',
-        //]);
-        Listing::factory(20)->create();
+      User::factory()->create([
+          'name' => 'Test User',
+          'email' => 'test@example.com',
+      ]);
+      
+      Listing::factory(20)->create();
     }
 }
