e74e15f320e80b24e0964251b1e7f614d9dacebc
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2024-12-01 15:34:16 +0000

    Monthly payment calcurator

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -4,20 +4,58 @@
             <div class="text-center w-full font-medium text-gray-500">No images</div>
         </Box>          
         <div class="md:col-span-5 flex flex-col gap-4">
-           <Box>
-            <template #header>
-                Basic Info
-            </template>
-            <Price :price="listing.price" class="text-2xl font-bold"></Price>
-            <ListingSpace :listing="listing" class="text-lg"/>
-            <ListingAddress :listing="listing" class="text-gray-500"/>
-           </Box>
+            <Box>
+                <template #header>
+                    Basic Info
+                </template>
+                <Price :price="listing.price" class="text-2xl font-bold"></Price>
+                <ListingSpace :listing="listing" class="text-lg"/>
+                <ListingAddress :listing="listing" class="text-gray-500"/>
+            </Box>
            
-           <Box>
-            <template #header>
-                Offer
-            </template>
-            Make an Offer
+            <Box>
+                <template #header>
+                    Monthly payment
+                </template>
+                <div>
+                    <label class="label">Interest rate({{ interestRate }}%)</label>
+                    <input v-model.number="interestRate"
+                    type="range" min="0.1" max="30" step="0.1"
+                    class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" 
+                    />
+
+                    <label class="label">Duration({{ duration }} years)</label>
+                    <input v-model.number="duration" 
+                    type="range" min="3" max="35" step="1"
+                    class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" 
+                    /> 
+                    
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">Your mothly payment</div>
+                        <Price :price="monthlyPayment" class="text-3xl" />
+                    </div>
+
+                    <div class="mt-2 text-gray-500">
+                        <div class="flex justify-between">
+                            <div>Total paid</div>
+                            <div>
+                                <Price :price="totalPaid" class="font-medium" />
+                            </div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div>Principal paid</div>
+                            <div>
+                                <Price :price="listing.price" class="font-medium" />
+                            </div>
+                        </div>
+                        <div class="flex justify-between">
+                            <div>Interest paid</div>
+                            <div>
+                                <Price :price="totalInterest" class="font-medium" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
            </Box>
         </div>    
     </div>
@@ -28,8 +66,17 @@
     import ListingSpace from '@/Components/ListingSpace.vue';
     import ListingAddress from '@/Components/ListingAddress.vue';
     import Box from '@/Components/UI/Box.vue';
+    import {ref} from 'vue';
+    import {useMonthlyPayment} from '@/Composables/useMonthlyPayment';
 
-    defineProps({
+    const interestRate = ref(2.5)
+    const duration = ref(25)
+
+    const props = defineProps({
         listing: Object
     })
+
+    const {monthlyPayment, totalPaid, totalInterest} = useMonthlyPayment(
+        props.listing.price, interestRate, duration
+        )
 </script>
