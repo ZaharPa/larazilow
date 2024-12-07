a34b16decaef14ec54011e933c6dca4ec58a3bd8
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2024-12-07 19:34:50 +0000

    email verification

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,10 +19,11 @@ class UserAccountController extends Controller
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8|confirmed'
         ]));
-        
         $user->save();
+        
         Auth::login($user);
-     
+        event(new Registered($user));
+
         return redirect()->route('listing.index')
             ->with('success', 'Account created!');
     }
